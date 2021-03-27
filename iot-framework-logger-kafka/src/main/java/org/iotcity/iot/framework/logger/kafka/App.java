cd214a4747976535fec782483b897c93d2845fa4
ardon <ind_nc@163.com>
2021-03-27 23:19:33 +0800

    Project initialization.

---
+++ iot-framework-logger-kafka/src/main/java/org/iotcity/iot/framework/logger/kafka/App.java
@@ -0,0 +1,13 @@
+package org.iotcity.iot.framework.logger.kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ iot-framework-logger-log4j/src/main/java/org/iotcity/iot/framework/logger/log4j/App.java
@@ -0,0 +1,13 @@
+package org.iotcity.iot.framework.logger.log4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
